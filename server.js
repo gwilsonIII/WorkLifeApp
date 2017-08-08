0b18a7cd33ee9cd84760fbdfdefe0ab6c342ce4b
gwilsonIII <gwilson2@alumni.nd.edu>
2017-08-08 18:33:04 -0400

    Added webpage to add contact info, updated other pages

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 var express = require('express');		//Creates an express object
 var mysql = require('./connect.js');	//Creates connect object (to connect Node to SQL)
 var app = express();					//Calls the express constructor
@@ -7,7 +6,7 @@ var request = require('request');
 app.set('port', 17941);					//Listens to any traffic at port 17941
 
 
-//GET request sends MySQL table 'employees' to whomever requested it.
+//GET request MySQL table employees
 app.get('/getTable', function(req, res, next) {
 
 		mysql.pool.query('SELECT * FROM employees', function(err, rows, fields) {
@@ -19,10 +18,37 @@ app.get('/getTable', function(req, res, next) {
 			var results = JSON.stringify(rows);
 			res.header('Access-Control-Allow-Origin', '*');
 			res.send(results);
-			
 		});
 });
 
+
+//GET request MySQL table workContacts
+app.get('/getWorkContacts', function(req, res, next) {
+
+		mysql.pool.query('SELECT * FROM workContacts', function(err, rows, fields) {
+			if(err) {
+				next(err);
+				return;
+			}
+		
+			var results = JSON.stringify(rows);
+			res.header('Access-Control-Allow-Origin', '*');
+			res.send(results);
+		});
+});
+
+//Add contact info from  addcontacts form to the employee info database
+app.get('/addcontacts.html',function(req,res,next){
+  var context = {};
+  mysql.pool.query("INSERT INTO workContacts (coID, fname, lname, email, phone) VALUES (?, ?, ?, ?, ?)", [req.query.coID, req.query.fname, req.query.lname, req.query.email, req.query.phone], function(err, result){
+    if(err){
+      next(err);
+      return;
+    }
+    context.results = "Inserted id " + result.insertId;
+    res.render('home',context);
+  });
+});
 
 //Error handling: 404
 app.use(function(req, res) {
